d4f33d9677d356d1a82c9aa16d85758a94c00769
Andrew <andrewbeshay2525@gmail.com>
2024-09-20 11:57:19 -0400

    setup testing

---
+++ app/main.py
@@ -0,0 +1,12 @@
+# addition function 
+def addition(a,b):
+    return a + b
+
+def subtraction(a,b):
+    return a - b
+
+def multiplication(a,b):
+    return a * b
+
+def division(a,b):
+    return a / b
---
+++ tests/test_main.py
@@ -0,0 +1,5 @@
+from app.main import addition
+
+# test_basic.py
+def test_addition():
+	assert addition(1,1) == 2

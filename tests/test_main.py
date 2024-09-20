d851bc78ed19963c4cafa7338d1fec3765376444
Andrew <andrewbeshay2525@gmail.com>
2024-09-20 12:12:30 -0400

    added more functionality testing

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,6 +1,25 @@
 '''My Calculator Test'''
-from app.main import addition
+
+import pytest
+from app.main import addition, division, multiplication, subtraction
 
 def test_addition():
     '''Addition function'''
     assert addition(1,1) == 2
+
+def test_subtraction():
+    '''Subtraction function'''
+    assert subtraction(1,1)  == 0
+
+def test_multiplication():
+    '''Multiplication function'''
+    assert multiplication(1,2)  == 2
+
+def test_positive_division():
+    '''Positive Division Test'''
+    assert division(1,1) == 1
+
+def test_negative_division():
+    '''Negative Division Test'''
+    with pytest.raises(ZeroDivisionError):
+        division(10, 0)
